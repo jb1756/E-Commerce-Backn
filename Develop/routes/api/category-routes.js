6ea03c7be91bb749b4d1cdced32d69891cd68ccf
jb1756 <juliusbags04@yahoo.com>
2023-07-13 18:30:56 -0400

    category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,33 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+    try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product }],
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'Category not found' });
+        return;
+      }
+  
+      res.json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
